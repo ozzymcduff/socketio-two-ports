6dc4655a00f38d4157fa71d731257fd0d5b4710d
Oskar Gewalli <gewalli@gmail.com>
2013-02-12 19:45:49 +0100

    using jsonp since ff does not like cross port requests

--- socket_server.js
+++ socket_server.js
@@ -1,21 +1,34 @@
 var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
-  , url = require('url');
+  , url = require('url')
+  , _ = require('underscore');
 
 app.listen(8099);
 
 function handler (req, res) {
 	var parsed = url.parse(req.url);
 	if (parsed.pathname==='/pingjs'){
+		//sloppy use of variable declarations
+		var callback = _.find( _.map(parsed.query.split('&'),function(s){
+			var p = s.split('=');
+			return { k:p[0], v:p[1] };
+		}), function(s){
+			return s.k==='callback'
+		});
+		console.log(callback);
 		res.writeHead(200,{
          'Content-Type': 'application/json',
          //http://devlicio.us/blogs/derik_whittaker/archive/2013/02/10/help-i-am-getting-an-xmlhttprequest-exception-of-access-control-allow-orig.aspx
-         'Access-Control-Allow-Origin':'http://localhost:8100'
+         // does not work in ff
+         'Access-Control-Allow-Origin':'http://localhost:8100',
      	});
-    	res.end(JSON.stringify({test:'some json'}));
+     	if (callback){
+     		res.end(callback.v+'('+ JSON.stringify({test:'some json'})+')');
+     	}else{
+	    	res.end(JSON.stringify({test:'some json'}));
+     	}
 	}else{
-		//console.log(req);
     	res.writeHead(200);
     	res.end("server");		
 	}
